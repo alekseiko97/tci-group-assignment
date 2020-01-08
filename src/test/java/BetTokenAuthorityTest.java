2ce947acb3e401f8abb7f669ce4e42bce04e41f0
sharique03 <mdanwersharique@gmail.com>
2020-01-08 16:01:36 +0100

    GamingMachineTest & BetTokenAuthorityTest classes are added

---
+++ src/test/java/BetTokenAuthorityTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+public class  BetTokenAuthorityTest {
+
+
+    /**
+     *test should pass when betting authority successfully logs bet tokens with their timestamp
+     *testing method Void logByBetTokenAuthority(string timeStamp, string info)
+     */
+    @Test
+    public void successfullyLogBetTokenAuthority()
+    {
+    }
+
+}
