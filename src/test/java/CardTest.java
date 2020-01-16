5fa0cc4009f1357f3fd3b9e6f32f15ba81b976b3
sharique03 <mdanwersharique@gmail.com>
2020-01-16 12:39:23 +0100

    cardSuccessfullyReturnedToCashier

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,14 +1,28 @@
+import org.junit.Assert;
 import org.junit.Test;
 
-public class GamblerTest {
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static org.mockito.Mockito.when;
+
+public class CardTest {
 
     /**
      *test should pass when provided card is returned to cashier
-     *testing method void returnCardToCashier(Card card)
+     *testing method void returnCardToCashier()
      */
     @Test
-    public void cardSuccessfullyReturnsToCashier()
+    public void cardSuccessfullyReturnedToCashier()
     {
+        //arrange
+        boolean isReturnedTrue = true;
+        Cashier cashier = mock(Cashier.class);
+        Card card = cashier.issueCard();
+        //act
+        boolean result = card.returnCardToCashier();
+        //assert
+        Assert.assertEquals(isReturnedTrue,result);
     }
 
     /**
@@ -18,6 +32,7 @@ public class GamblerTest {
     @Test
     public void successfullyDepositMoneyInCard()
     {
+
     }
 
 
