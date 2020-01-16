e11a924efd4a733e5691fa32fa6abdaf3d22f930
Thao <thuthaovinh@gmail.com>
2020-01-16 14:48:57 +0100

    Refactor test method placeBetInBetRoundShouldReturnBetValueSuccessful

--- src/main/java/BetRound.java
+++ src/main/java/BetRound.java
@@ -8,4 +8,8 @@ public class BetRound {
     public LocalDateTime endRound(Bet bet) {
         return LocalDateTime.now();
     }
+
+    public Double placeBet(Bet bet, double v) {
+        return v;
+    }
 }
