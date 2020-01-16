79342ff8447a096af5d33666a2608f12c30efddb
Thao <thuthaovinh@gmail.com>
2020-01-16 14:34:33 +0100

    Refactor test method endingBetRoundShouldReturnDateSuccessful

--- src/main/java/BetRound.java
+++ src/main/java/BetRound.java
@@ -4,4 +4,8 @@ public class BetRound {
     public LocalDateTime startRound(Bet bet) {
         return LocalDateTime.now();
     }
+
+    public LocalDateTime endRound(Bet bet) {
+        return LocalDateTime.now();
+    }
 }
