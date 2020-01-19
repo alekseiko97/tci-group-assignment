6154bbe5e0573cf3f61fec5c23821bd503ba178f
Thao <thuthaovinh@gmail.com>
2020-01-19 14:08:17 +0100

    Create test method betPlacedWithSufficientAmountShouldThrowException

--- src/test/java/BetTest.java
+++ src/test/java/BetTest.java
@@ -1,4 +1,7 @@
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.UUID;
 
 public class BetTest {
     /**
@@ -12,5 +15,33 @@ public class BetTest {
         //act
         Bet bet = new Bet(insufficientBetAmount);
         //assert
+    }
+
+    /**
+     * This test should be passed when a bet is placed with sufficient amount
+     * Test is created to test the constructor Bet(double inAmount)
+     */
+    @Test
+    public void betPlacedWithSufficientAmountShouldThrowException(){
+        //arrange
+        double insufficientBetAmount = 20;
+        //act
+        Bet bet = new Bet(insufficientBetAmount);
+        //assert
+    }
+
+    /**
+     * This test should be passed when betId of a bet is returned
+     * Test is created to test the method UUID getBetId()
+     */
+    @Test
+    public void betIdOfABetShouldBeReturnedSuccessful(){
+        //arrange
+        double sufficientAmount = 20;
+        Bet bet = new Bet(sufficientAmount);
+        //act
+        UUID actualBetId = bet.getBetId();
+        //assert
+        Assert.assertNotNull("BetId is not get correctly",actualBetId);
     }
 }
