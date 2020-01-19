5ab4737b5d01d8403f04ace9ad2348c5b55cf88e
sharique03 <mdanwersharique@gmail.com>
2020-01-19 17:19:59 +0100

    fail handingInGamblingCardShouldBeLogged

--- src/test/java/BettingAuthorityTest.java
+++ src/test/java/BettingAuthorityTest.java
@@ -1,10 +1,40 @@
+import javafx.scene.chart.PieChart;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.mockito.Mockito.mock;
 
 
 public class BettingAuthorityTest {
 
+    private BettingAuthority bettingAuthority;
+    private String token;
+    private String timestamp;
+    private List<String> countOfLogs;
+    private Casino casino;
+    private BetRound betRound;
+    private Bet bet;
+    private Card card;
+    //setup method will be executed first before test
+    @Before
+    public void setUp(){
+        bettingAuthority = new BettingAuthority();
+        casino = mock(Casino.class);
+        countOfLogs = new ArrayList<>();
+        token = UUID.randomUUID().toString();
+        timestamp = LocalDateTime.now().toString();
+        betRound = mock(BetRound.class);
+        bet = mock(Bet.class);
+        card = mock(Card.class);
+    }
 
     /**
      * Test should pass when starting betting round is accompanied by a logging and timestamping of it
@@ -13,17 +43,24 @@ public class BettingAuthorityTest {
      */
     @Test
     public void creationOfBettingRoundShouldBeLoggedAndTimestamped() {
-        throw new NotImplementedException();
+        //arrange
+        //act
+        bettingAuthority.logBettingRound(token,timestamp);
+        //assert
+        assertEquals(1,bettingAuthority.loggerList.size());
     }
         /**
          * Test should pass if all bets from the given betting round are logged by the end of the round
          * Valid token should be provided to succeed
          * This test is created to test void logEnd(List<Bet> bets, String token)
          */
-
     @Test
     public void allBetsFromBettingRoundShouldBeLoggedWhenFinished() {
-        throw new NotImplementedException();
+        //arrange
+        //act
+        bettingAuthority.logEnd(betRound,bet,timestamp);
+        //assert
+        assertEquals(1, bettingAuthority.loggerList.size());
     }
 
     /**
@@ -32,8 +69,13 @@ public class BettingAuthorityTest {
      */
 
     @Test
-    public void handingOutGamblingCardShouldBeLogged() {
-        throw new NotImplementedException();
+    public void handingOutGamblingCardShouldBeLogged()
+    {
+        //arrange
+        //act
+        bettingAuthority.logCardHandedOut(card, timestamp);
+        //assert
+        assertEquals(1,bettingAuthority.loggerList.size());
     }
 
     /**
@@ -42,8 +84,13 @@ public class BettingAuthorityTest {
      */
 
     @Test
-    public void handingInGamblingCardShouldBeLogged() {
-        throw new NotImplementedException();
+    public void handingInGamblingCardShouldBeLogged()
+    {
+        //arrange
+        //act
+        bettingAuthority.logCardReturned(card, timestamp);
+        //assert
+        assertEquals(1,bettingAuthority.loggerList.size());
     }
 
     /**
